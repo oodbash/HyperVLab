4fc078ce097614d9a4dafa8cfaf85b7cd092c271
oodbash <oodbash@outlook.com>
2022-05-10 14:15:15 +0200

    ...

---
+++ Configure-VM.ps1
@@ -0,0 +1,27 @@
+#Declare variables
+$ipaddress = "192.168.1.11" 
+$ipprefix = "20" 
+$ipgw = "192.168.0.1" 
+$ipdns = "192.168.1.11"
+$ipdns2 = "8.8.8.8" 
+$ipif = (Get-NetAdapter).ifIndex 
+$featureLogPath = "c:\poshlog\featurelog.txt" 
+$newname = "phs-dc-01"
+$addsTools = "RSAT-AD-Tools" 
+
+#Set static IP address
+New-NetIPAddress -IPAddress $ipaddress -PrefixLength $ipprefix -InterfaceIndex $ipif -DefaultGateway $ipgw 
+
+# Set the DNS servers
+Set-DnsClientServerAddress -InterfaceIndex $ipif -ServerAddresses ($ipdns, $ipdns2)
+
+#Rename the computer 
+Rename-Computer -NewName $newname -force 
+
+#Install features 
+New-Item $featureLogPath -ItemType file -Force 
+Add-WindowsFeature $addsTools 
+Get-WindowsFeature | Where installed >>$featureLogPath 
+
+#Restart the computer 
+Restart-Computer
--- Create-VM.ps1
+++ Create-VM.ps1
@@ -1,22 +1,21 @@
 #Declare variables
 $VMName = 'phs-dc-01'
 $Switch = 'NATSwitch'
-$InstallMedia = 'C:\ISOs\en_windows_server_2016_updated_feb_2018_x64_dvd_11636692.iso'
+$InstallMedia = 'C:\ISOs\en-us_windows_server_2019_updated_aug_2021_x64_dvd_a6431a28.iso'
 $Path = 'X:\hyper-v'
-$VHDPath = 'X:\hyper-v\virtual hard disks\phs-dc-01.vhdx'
+$VHDPath = 'X:\hyper-v\Virtual Hard Disks\phs-dc-01.vhdx'
 $VHDSize = '64424509440'
-
 #Create New Virtual Machine
-New-VM -Name $VMName -MemoryStartupBytes 8GB -BootDevice VHD -Path $Path -NewVHDPath $VHDPath -NewVHDSizeBytes $VHDSize  -Generation 2 -Switch $Switch  
-
+New-VM -Name $VMName -MemoryStartupBytes 8GB -BootDevice VHD -NewVHDPath $VHDPath -NewVHDSizeBytes $VHDSize  -Generation 2 -Switch $Switch  
 #Set the memory to be non-dynamic
 set-vm -Name "phs-dc-01" -DynamicMemory -ProcessorCount 2 -checkpointtype disabled 
-
 #Add DVD Drive to Virtual Machine
 Add-VMDvdDrive -VMName $VMName -ControllerNumber 0 -ControllerLocation 1 -Path $InstallMedia
-
 #Mount Installation Media
 $DVDDrive = Get-VMDvdDrive -VMName $VMName
-
 #Configure Virtual Machine to Boot from DVD
-Set-VMFirmware -VMName $VMName -FirstBootDevice $DVDDrive 
+$VMFW = Get-VMFirmware -VMName $VMName
+$dvd=$vmfw.bootorder[2] 
+$pxe=$vmfw.bootorder[0] 
+$hdd=$vmfw.bootorder[1] 
+Set-VMFirmware -vmname $VMName -BootOrder $dvd,$hdd,$pxe
